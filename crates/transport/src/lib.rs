54c080b49a4d6fe0daac0efbba30306447c8b19d
abdulrahman hassan <h4554n.abdul@gmail.com>
2026-01-23 10:29:49 +0100

    transport: lib

--- crates/transport/src/lib.rs
+++ crates/transport/src/lib.rs
@@ -1,12 +1,24 @@
+pub mod tcp;
+pub mod tls;
+pub mod tls_config;
+pub mod acceptor;
+pub mod error;
+pub mod metrics;
+
+pub use acceptor::SentinelAcceptor;
+pub use error::{TransportError, TransportResult};
+pub use tcp::RawTcpTransport;
+pub use tls::TlsTransport;
+
 use async_trait::async_trait;
 use tokio::io::{AsyncRead, AsyncWrite};
 use std::net::SocketAddr;
 
 #[async_trait]
 pub trait SentinelTransport: AsyncRead + AsyncWrite + Unpin + Send {
-    /// returns the remote address of the peer.
+    /// Returns the remote address of the peer.
     fn peer_addr(&self) -> Result<SocketAddr, std::io::Error>;
 
-    /// returns true if the transport is encrypted (TLS).
+    /// Returns true if the transport is encrypted (TLS).
     fn is_secure(&self) -> bool;
 }

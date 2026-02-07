6ab6b8b83bef1e47ff9240a6f56086abc0e0fb33
abdulrahman hassan <h4554n.abdul@gmail.com>
2026-02-07 13:40:28 +0100

    main

--- crates/sentinel-signaler/src/main.rs
+++ crates/sentinel-signaler/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+use std::sync::Arc;
+use dashmap::DashMap;
+use std::net::SocketAddr;
+
+//  hold active nodes
+type PeerDirectory = Arc<DashMap<String, SocketAddr>>;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    env_logger::init();
+    let addr = "0.0.0.0:8888"; // The public meeting point
+    let listener = TcpListener::bind(addr).await?;
+    let directory: PeerDirectory = Arc::new(DashMap::new());
+
+    println!("🛰️ SENTINEL SIGNALER live on {}", addr);
+
+    loop {
+        let (socket, peer_addr) = listener.accept().await?;
+        let dir = Arc::clone(&directory);
+
+        tokio::spawn(async move {
+            println!(" Discovery: Node connected from {}", peer_addr);
+            // implement the Handshake 
+            // to verify the Node's Identity and add them to the directory.
+        });
+    }
 }
